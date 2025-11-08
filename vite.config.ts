b672685d73981127dde3fcd04175798d3097a04a
Antipas Henry <78890228+Igult2017@users.noreply.github.com>
2025-11-08 23:41:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// ES-module-safe __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
@@ -26,3 +31,4 @@ export default defineConfig({
     },
   },
 });
+
